5064d609622305bc87bb1fce09ac1f1cb2e441a9
An071003 <adpqt2003@gmail.com>
2024-12-09 19:38:04 +0700

    first_commit

--- README.md
+++ README.md
@@ -1,1 +1,61 @@
-# IS353_Mangxahoi_nhom6
+# ReadMe.txt
+## Tác giả:
+"Hồ Vũ An - 21521804"
+"Dương Uy Quan - 21521323"
+"Lê Thanh Phong - 21521271"
+"Nguyễn Thành Trung - 21521595"
+"Phạm Tiến Đạt - 21521948"
+***
+## 1. Giới thiệu chung
+Đây là đồ án cho môn học IS353 - Mạng xã hội - Nhóm 6
+Đồ án này sẽ thực hiện các chạy các mô hình như là KGAT, Factorization Machine, Neural Factorization Machine, Bayesian Personalized Ranking Matrix Factorization trên bộ dữ liệu uit(được cung cấp).
+Đồ án có sự tham khảo bởi 2 đồ án khác:
+        [Hệ thống đề xuất cho MOOC](https://drive.google.com/drive/folders/1DZPvcWluXIlHK-LyXls4Ej4ze8BPH2yo)
+        [Hệ thống đề xuất cho UIT](https://drive.google.com/drive/folders/1E2zw9jS5222i8XKKGzWYlRsALtJYvLX_)
+Các mô hình sử dụng được lấy từ link: https://github.com/LunaBlack/KGAT-pytorch?tab=readme-ov-file#environment-requirement
+***
+## 2. Các mô hình trong dự án
+### 2.1. KGAT (Knowledge Graph Attention Network)
+KGAT là một mô hình học sâu kết hợp giữa mạng nơ-ron và đồ thị tri thức. Nó được thiết kế để xử lý các bài toán học máy với dữ liệu có cấu trúc đồ thị. Các tính năng nổi bật của KGAT:
+
+Kết hợp các thông tin về người dùng và sản phẩm từ đồ thị tri thức.
+Sử dụng cơ chế Attention để học được các trọng số quan trọng của các nút trong đồ thị.
+Phù hợp với các bài toán gợi ý trong đó các đối tượng có quan hệ với nhau thông qua đồ thị (ví dụ: người dùng, sản phẩm, danh mục).
+### 2.2. FM (Factorization Machine)
+FM là một mô hình học máy được thiết kế để giải quyết các bài toán học máy trong môi trường có nhiều đặc tính tương tác (ví dụ: các đặc tính của người dùng và sản phẩm). FM có khả năng học được mối quan hệ không tuyến tính giữa các đặc tính, điều này giúp mô hình hoạt động tốt trong các bài toán gợi ý.
+
+FM có thể học các mối quan hệ giữa các đặc tính mà không cần tính toán quá phức tạp, rất hiệu quả cho bài toán hệ thống đề xuất.
+Mô hình này giúp giảm thiểu số lượng các tham số so với các mô hình khác như mạng nơ-ron, mà vẫn đạt được hiệu suất khá cao.
+### 2.3. NFM (Neural Factorization Machine)
+NFM là một mở rộng của mô hình FM nhưng kết hợp với mạng nơ-ron để tăng cường khả năng học các mối quan hệ không tuyến tính. Trong NFM, các tính năng của người dùng và sản phẩm được kết hợp qua một mạng nơ-ron để học các tương tác phức tạp hơn.
+
+NFM kết hợp các đặc điểm của FM và học sâu (deep learning).
+Mô hình này có khả năng cải thiện độ chính xác của dự đoán gợi ý nhờ vào việc sử dụng mạng nơ-ron để học các biểu diễn ẩn của các đặc tính.
+### 2.4. BPRMF (Bayesian Personalized Ranking Matrix Factorization)
+BPRMF là một mô hình ma trận phân rã được sử dụng để tối ưu hóa các hệ thống đề xuất dựa trên Bayesian Personalized Ranking. BPRMF tối ưu hóa các thứ hạng của các mục trong hệ thống gợi ý thay vì chỉ đơn giản là dự đoán điểm số của người dùng đối với các mục.
+
+BPRMF sử dụng phương pháp Bayesian để tối ưu hóa thứ hạng của các đối tượng trong ma trận phân rã.
+Mô hình này rất hiệu quả trong các bài toán gợi ý mà mục tiêu là xếp hạng các sản phẩm thay vì dự đoán điểm số chính xác.
+***
+# 3. Môi trường
+code được chạy với Python 3.9 và các thư viện liên quan:
+```
+torch == 2.5.1
+numpy == 2.1.1
+pandas == 1.3.5
+scipy == 1.14.1
+polars == 1.17.0
+tqdm == 4.67.0
+scikit-learn == 1.5.2
+```
+Chi tiết trong file requirements.txt
+***
+4. Chạy code
+- Train
+```
+python file_main_mô_hình.py --model_type fm --data_name amazon-book
+```
+- Predict
+```
+python file_predict_mô_hình.py --model_type fm --data_name amazon-book
+```
---
+++ Web-MXH-Nhóm6/main.py
@@ -0,0 +1,167 @@
+import streamlit as st
+import numpy as np
+import polars as pl
+import pandas as pd
+import json
+
+# Đọc tệp user_list.txt và lọc người dùng theo điều kiện
+def load_filtered_users(user_list_file_path):
+    filtered_users = {}
+    try:
+        with open(user_list_file_path, 'r', encoding='utf-8') as f:
+            for line in f:
+                # Bỏ qua dòng tiêu đề
+                if 'org_id' in line or 'remap_id' in line:
+                    continue
+                parts = line.strip().split()
+                if len(parts) >= 13:  # Đảm bảo đủ cột dữ liệu
+                    org_id = parts[0]  # Không chuyển đổi org_id
+                    try:
+                        remap_id = int(parts[1])  # remap_id là số nguyên
+                        sinhvien_nam = int(parts[12])  # Lọc theo sinhvien_nam
+                        if sinhvien_nam >= 3:
+                            filtered_users[org_id] = remap_id
+                    except ValueError:
+                        print(f"Không thể chuyển đổi giá trị trong dòng: {line}")
+    except FileNotFoundError:
+        print(f"Tệp {user_list_file_path} không tồn tại.")
+    return filtered_users
+
+# Hàm lấy top 5 items tương tự như trước nhưng có ánh xạ từ filtered_users
+def get_top_5_items_filtered(file_path, filtered_users, item_mapping_file_path, mssv_input):
+    try:
+        scores = np.load(file_path)
+    except FileNotFoundError:
+        print(f"Tệp {file_path} không tồn tại.")
+        return []
+
+    user_index = filtered_users[mssv_input]
+
+    if len(scores) <= user_index:
+        print("Chỉ số user không hợp lệ.")
+        return []
+
+    user_scores = scores[user_index]  # Lấy điểm của user
+    top_5_items = np.argsort(user_scores)[::-1][:5]  # Lấy 5 item có điểm số cao nhất
+
+    # Tải ánh xạ item
+    remap_dict = {}
+    try:
+        with open(item_mapping_file_path, 'r', encoding='utf-8') as f:
+            for line in f:
+                if 'remap_id' in line or 'org_id' in line:
+                    continue
+                parts = line.strip().split()
+                if len(parts) == 2:
+                    try:
+                        remap_id = int(parts[1])
+                        remap_dict[remap_id] = parts[0]
+                    except ValueError:
+                        continue
+    except FileNotFoundError:
+        print(f"Tệp ánh xạ {item_mapping_file_path} không tồn tại.")
+
+    result = []
+    for item_index in top_5_items:
+        item_name = remap_dict.get(item_index, f"Item {item_index + 1}")
+        result.append((item_name, user_scores[item_index]))
+
+    return result
+
+# Hiển thị top 5 môn học dựa trên file_path cụ thể.
+def display_top_5(file_path, filtered_users, item_mapping_file_path, mssv_input, title):
+    top_5_items = get_top_5_items_filtered(file_path, filtered_users, item_mapping_file_path, mssv_input)
+
+    if top_5_items:
+        st.write(f"{title}:")
+        data = pl.DataFrame({
+            "Mã môn học": [item[0].upper() for item in top_5_items],
+            "Scores": [item[1] for item in top_5_items],
+        })
+
+        st.table(data)
+    else:
+        st.write(f"{title}: Không có dữ liệu điểm hoặc sinh viên không đủ điều kiện.")
+
+# Hàm để mã hóa mssv_input với dữ liệu trong mssv.json và lấy mssv_raw
+def decode_mssv(mssv_input, mssv_json_file='mssv.json'):
+    try:
+        # Đọc dữ liệu từ file mssv.json
+        with open(mssv_json_file, 'r', encoding='utf-8') as f:
+            mssv_data = json.load(f)
+
+        # Kiểm tra nếu mssv_input có trong file
+        for value in mssv_data.values():
+            for item in value:
+                if item['mssv'] == mssv_input:
+                    mssv_raw = item['mssv_raw']
+                    print(mssv_raw)
+                    return mssv_raw
+        else:
+            st.sidebar.warning(f"MSSV {mssv_input} không có trong dữ liệu.")
+            return mssv_input
+    except FileNotFoundError:
+        print(f"File {mssv_json_file} không tồn tại.")
+        return None
+    except json.JSONDecodeError:
+        print(f"File {mssv_json_file} không phải là file JSON hợp lệ.")
+        return None
+
+
+# Hàm run để xử lý toàn bộ quá trình
+def run(mssv_input):
+    # Đường dẫn đến các tệp dữ liệu
+    user_list_file_path = "../notebooks/Model/KGAT-pytorch/KGAT_data/user_list.txt"
+    item_mapping_file_path = "../Data\Train_test_data\Data_mapping\mp_mamh.txt"
+
+    # Tải danh sách người dùng thỏa mãn điều kiện
+    filtered_users = load_filtered_users(user_list_file_path)
+
+    mssv_begin = mssv_input
+    if mssv_input != "":
+        mssv_input = decode_mssv(mssv_input)
+
+
+    if mssv_input == "":
+        # Nếu MSSV chưa nhập, hiển thị yêu cầu nhập MSSV
+        st.sidebar.success("Vui lòng nhập mã số sinh viên để tiếp tục.")
+    elif mssv_input not in filtered_users:
+        # Nếu MSSV không hợp lệ
+        st.sidebar.warning(f"MSSV {mssv_input} không có trong dữ liệu.")
+    else:
+        # Hiển thị giao diện Streamlit
+        st.title(f'Môn học đề xuất cho sinh viên {mssv_begin}')
+        file_paths = [
+            (
+                r"../notebooks\Model\KGAT-pytorch\trained_model\BPRMF\KGAT_data\embed-dim64_lr0.0001_pretrain2\cf_scores_bprmf.npy",
+                "Matrix Factorization (BPRMF)"),
+            (
+                r"../notebooks\Model\KGAT-pytorch\trained_model\NFM\KGAT_data\fm_embed-dim64_64-32-16_lr0.0001_pretrain2_user-info0\cf_scores_fm0.npy",
+                "Factorization Machine without user_info"),
+            (
+                r"../notebooks\Model\KGAT-pytorch\trained_model\NFM\KGAT_data\fm_embed-dim64_64-32-16_lr0.0001_pretrain2_user-info1\cf_scores_fm1.npy",
+                "Factorization Machine with user_info"),
+            (
+                r"../notebooks\Model\KGAT-pytorch\trained_model\NFM\KGAT_data\nfm_embed-dim64_64-32-16_lr0.0001_pretrain2_user-info0\cf_scores_nfm0.npy",
+                "Neural Factorization Machine (NFM) without user_ìnfo"),
+            (
+                r"../notebooks\Model\KGAT-pytorch\trained_model\NFM\KGAT_data\nfm_embed-dim64_64-32-16_lr0.0001_pretrain2_user-info0\cf_scores_nfm0.npy",
+                "Neural Factorization Machine (NFM) with user_info"),
+            (
+                r"../notebooks\Model\KGAT-pytorch\trained_model\KGAT\KGAT_data\embed-dim64_relation-dim64_random-walk_bi-interaction_64-32-16_lr0.0001_pretrain2\cf_scores_kgat.npy",
+                "KGAT"),
+            (
+                r"../notebooks\Model\KGAT-pytorch\trained_model\KGAT\KGAT_data\embed-dim64_relation-dim64_random-walk_bi-interaction_64-32-16_lr0.0001_pretrain2\cf_scores_embed.npy",
+                "KGAT EMBED"),
+        ]
+
+        for file_path, title in file_paths:
+            display_top_5(file_path, filtered_users, item_mapping_file_path, mssv_input, title)
+
+
+if __name__ == "__main__":
+    # Sidebar để nhập MSSV
+    st.sidebar.header("Đăng nhập")
+    mssv_input = st.sidebar.text_input("Nhập mã số sinh viên:", value="")
+
+    run(mssv_input)
