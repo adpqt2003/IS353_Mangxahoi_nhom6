4a8d9f391b0d5e9c568d9ad91ffca97cb2062f41
adpqt2003 <adpqt2003@gmail.com>
2024-12-09 21:44:19 +0700

    Update main.py

--- Web-MXH-Nhóm6/main.py
+++ Web-MXH-Nhóm6/main.py
@@ -112,7 +112,7 @@ def decode_mssv(mssv_input, mssv_json_file='mssv.json'):
 # Hàm run để xử lý toàn bộ quá trình
 def run(mssv_input):
     # Đường dẫn đến các tệp dữ liệu
-    user_list_file_path = "./notebooks/Model/KGAT-pytorch/KGAT_data/user_list.txt"
+    user_list_file_path = ""..\\notebooks\\Model\\KGAT-pytorch\\KGAT_data\\user_list.txt""
     item_mapping_file_path = "./Data\Train_test_data\Data_mapping\mp_mamh.txt"
 
     # Tải danh sách người dùng thỏa mãn điều kiện
